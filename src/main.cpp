74072e7096fbbe1849c24f5704f816263d691a22
w1esther <w1mariaesther@gmail.com>
2026-09-23 20:54:43 -0300

    atualizando robohardware

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include "Estrategia.h"
+#include <robo_hardware2.h>
 
 Estrategia estrategia;
 
-void setup() {
 
+void setup() {
+  robo.configurar();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  estrategia.executar();
 }
